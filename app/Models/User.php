a132a4ac58a1d0af337897434a5775f9a7b76252
KOU050223 <kouhei20050223@icloud.com>
2024-10-14 23:04:02 +0900

    add rooms_table room_user_table

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function room()
+    {
+        return $this->belongsToMany(Room::class)->withTimestamps();
+    }
 }
