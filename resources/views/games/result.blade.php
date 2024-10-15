0821ae217bfcdecbaa425d3dc7cd0dbb5fa31346
misqki00 <hagen.daru08@gmail.com>
2024-10-15 22:46:11 +0900

    結果画面作成

--- resources/views/games/result.blade.php
+++ resources/views/games/result.blade.php
@@ -4,14 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>マッチング画面</title>
+    <title>結果画面</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 
 <body>
     <div>
-        <h1>マッチング中...</h1>
-        <p>他のプレイヤーを待っています</p>
+        <h1>結果</h1>
     </div>
 </body>
 
