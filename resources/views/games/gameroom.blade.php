75bde1f6fd574aaefc37aa88757ece9234b4c61f
porChe1223 <houshinri@gmail.com>
2024-10-17 00:59:01 +0900

    チャットルームの作成（チャット機能はデモ版に入れずに後回しでよい）

--- resources/views/games/gameroom.blade.php
+++ resources/views/games/gameroom.blade.php
@@ -7,8 +7,11 @@
         <link rel="stylesheet" href="{{ asset('/css/gameroom.blade.css')}}">
     </head>
     <body>
+        <script src="result.js"></script>
+
         <div id="all-container">
             <h1>GameRoom</h1>
+            <!-- ゲーム指示 -->
             <div id="instructions-container">
                 <div id="title-container">
                     <div class="instructions">お題:</div>
@@ -23,9 +26,29 @@
                     </span>
                 </div>
             </div>
-            <div id="chat-room">
-                チャットルーム作成する
-            </div>
+            <h1>小さい順に並べよう!!</h1>
+            <!-- 以下はチャットルーム -->
+            <!-- <div id="chat-container">
+                <div id="chat-left">
+                    <div class="chat-text">
+                        左側のチャットメッセージ
+                    </div>
+                </div>
+                <div class="chat-clear"></div>
+                <div id="chat-right">
+                    <div class="chat-text">
+                        右側のチャットメッセージ
+                    </div>
+                </div>
+                <div class="chat-clear"></div>
+                <div id="chat-send-container">
+                    <input id="chat-message-text" type="text"></input>
+                    <button id="chat-message-send-button">送信</button>
+                </div>
+            </div> -->
+
+            <button type="submit" class="go-result-button">結果を見る</button>
+            
         </div>
 
         <div>
