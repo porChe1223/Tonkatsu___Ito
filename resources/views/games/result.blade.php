9b02da7d447b64027c51ee233860a80c33606ea7
misqki00 <hagen.daru08@gmail.com>
2024-10-18 19:49:19 +0900

    gameroomと結果画面装飾

--- resources/views/games/result.blade.php
+++ resources/views/games/result.blade.php
@@ -1,18 +1,19 @@
 <!DOCTYPE html>
 <html lang="Ja">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>結果画面</title>
-        <link rel="stylesheet" href="{{ asset('css/result.blade.css') }}">
-    </head>
 
-    <body>
-        <div>
-            <h1>答え</h1>
-            @foreach ($usersCardNumbers as $each_number)
-                {{$each_number}}
-            @endforeach
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>結果画面</title>
+    <link rel="stylesheet" href="{{ asset('css/result.blade.css') }}">
+</head>
+
+<body>
+    <div>
+        <h1>答え</h1>
+        @foreach ($usersCardNumbers as $each_number)
+        <span class="card-number">{{$each_number}}</span>
+        @endforeach
+</body>
 
 </html>
