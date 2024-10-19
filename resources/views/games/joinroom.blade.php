d003927123e50873add1afdc3b411012b19e369b
nitamagoGoGo <nitamagorain@gmail.com>
2024-10-20 00:13:35 +0900

    heyabanngou

--- resources/views/games/joinroom.blade.php
+++ resources/views/games/joinroom.blade.php
@@ -4,13 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>部屋番号待機画面</title>
+    <title>部屋番号検索画面</title>
     <link rel="stylesheet" href="{{ asset('/css/joinroom.blade.css')}}">
 </head>
 <body>
     <div class="container">
         <h1>部屋番号を入力して下さい</h1>
-        <input type="number">
+        <input type="number" id="roomId" required>
+        <button type="submit" class="btn btn-primary">検索</button>
         <!-- 部屋番号(=roomid)を入力してその部屋に参加させたい -->
 </body>
 </html>
