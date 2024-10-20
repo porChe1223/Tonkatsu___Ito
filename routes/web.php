a3139144dff24c1c0b960238473012db296897e9
porChe1223 <162541772+porChe1223@users.noreply.github.com>
2024-10-20 17:53:31 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
-    ->middleware('auth')
-    ->name('logout');
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->middleware('auth')->name('logout');
 });
 
 //ララベル画面
